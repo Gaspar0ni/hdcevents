d3994e6e7ada97915269fd120d3a11afb280016b
Guilherme <guilherme.vgp@outlook.com>
2022-03-17 19:03:52 -0300

    Avançando na template engine Blade (loop)

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,17 @@ Route::get('/', function () {
     $nome = "Matheus";
     $idade = 29;
 
-    return view('welcome', ['nome' => $nome, 'idade' => $idade, 'profissao' => "programador"]);
+    $arr = [10,20,30,40,50];
+
+    $nomes = ["Matheus", "Maria", "João", "Saulo"];
+
+    return view('welcome', [
+        'nome' => $nome, 
+        'idade' => $idade, 
+        'profissao' => "programador",
+        'arr' => $arr,
+        'nomes' => $nomes
+    ]);
 });
 
 Route::get('/contact', function () {
